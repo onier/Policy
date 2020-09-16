9c35bbcfff1cf3d740dc0a2b19fac8f498de2ce4
onier <282052309@qq.com>
2020-09-16 17:26:49 +0800

    Update BehaviorSystem.h

--- src/BehaviorSystem.h
+++ src/BehaviorSystem.h
@@ -1,3 +1,49 @@
+//#include <boost/shared_ptr.hpp>
+//#include <boost/type_traits/is_pointer.hpp>
+//#include <boost/mpl/bool.hpp>
+//#include <iostream>
+//#include <boost/core/enable_if.hpp>
+//#include <logger/Logger.h>
+//#include "rw/common/Ptr.hpp"
+//#include "rw/math/Transform3D.hpp"
+//
+//template<class T>
+//struct is_shared_ptr
+//        : boost::mpl::false_ {
+//};
+//
+//template<class T>
+//struct is_shared_ptr<boost::shared_ptr<T> >
+//        : boost::mpl::true_ {
+//};
+//
+//template<class T>
+//struct is_shared_ptr<rw::common::Ptr<T> >
+//        : boost::mpl::true_ {
+//};
+//
+//class Dummy {
+//};
+//
+//template<typename _T,
+//        typename std::enable_if<is_shared_ptr<_T>::value || std::is_same<_T, Dummy>::value, int>::type = 0
+//>
+//void check(_T) {
+//    LOG_INFO << "type pass";
+//}
+//
+//int main() {
+//    Dummy dummy;
+//    Dummy *dummy_ptr = &dummy;
+//    boost::shared_ptr<Dummy> shared_dummy(new Dummy);
+//
+//    check(dummy);        // false
+////    check(dummy_ptr);    // false
+//    check(shared_dummy); // true
+//    check(rw::common::ownedPtr(new rw::math::Transform3D<>()));
+//    return 0;
+//}
+//
 //
 // Created by dev on 2020/8/20.
 //
